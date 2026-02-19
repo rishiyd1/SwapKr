96de6b21c62acb09da8bbc00688c1e215cd86b3d
Rishi <risydv321@gmail.com>
2026-02-19 20:46:35 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.set("io", io);
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:8080", "http://127.0.0.1:8080"],
+    origin: ["http://localhost:8080", "http://127.0.0.1:8080", "https://swapkr.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   }),
